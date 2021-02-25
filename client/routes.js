ffb3b0d6604f3e3fcc06d0d4904e8e97f7fee3fc
shapirodaniel <shapirodanieladam@gmail.com>
2021-02-24 18:54:30 -0600

    fix: remove defunct components

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, Room} from './components'
+import {Login, Signup, UserHome, PeerManager} from './components'
 import {me} from './store'
 
 /**
@@ -25,7 +25,7 @@ class Routes extends Component {
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
-            <Route path="/rooms/:room" component={Room} />
+            <Route path="/rooms/:room" component={PeerManager} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
