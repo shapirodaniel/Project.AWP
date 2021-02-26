cb5c31738c5e6fc0e87df833696393e2c2cbba7d
shapirodaniel <shapirodanieladam@gmail.com>
2021-02-26 09:59:39 -0600

    fix: update README

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,59 +1,59 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../store";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <div className="topline-banner">
-      <Link to="/home">
-        <img src="https://i.imgur.com/E5nkxzt.png" />
-      </Link>
-      <div>
-        <Link to="/home">
-          <div className="titleCopy">
-            <h1>REDFISH / BLUEFISH</h1>
-            <span>a WebRTC video-chat app</span>
-          </div>
-        </Link>
-        <nav>
-          {isLoggedIn ? (
-            <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>
-                Logout
-              </a>
-            </div>
-          ) : (
-            <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-          )}
-        </nav>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div className='topline-banner'>
+			<Link to='/home'>
+				<img src='https://i.imgur.com/E5nkxzt.png' />
+			</Link>
+			<div>
+				<Link to='/home'>
+					<div className='titleCopy'>
+						<h1>REDFISH / BLUEFISH</h1>
+						<span>a WebRTC video-chat app</span>
+					</div>
+				</Link>
+				<nav>
+					{isLoggedIn ? (
+						<div>
+							{/* The navbar will show these links after you log in */}
+							<Link to='/home'>Home</Link>
+							<a href='#' onClick={handleClick}>
+								Logout
+							</a>
+						</div>
+					) : (
+						<div>
+							{/* The navbar will show these links before you log in */}
+							<Link to='/login'>Login</Link>
+							<Link to='/signup'>Sign Up</Link>
+						</div>
+					)}
+				</nav>
+			</div>
+		</div>
+	</div>
 );
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
-  return {
-    isLoggedIn: !!state.user.id,
-  };
+const mapState = state => {
+	return {
+		isLoggedIn: !!state.user.id,
+	};
 };
 
-const mapDispatch = (dispatch) => {
-  return {
-    handleClick() {
-      dispatch(logout());
-    },
-  };
+const mapDispatch = dispatch => {
+	return {
+		handleClick() {
+			dispatch(logout());
+		},
+	};
 };
 
 export default connect(mapState, mapDispatch)(Navbar);
@@ -62,6 +62,6 @@ export default connect(mapState, mapDispatch)(Navbar);
  * PROP TYPES
  */
 Navbar.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
+	handleClick: PropTypes.func.isRequired,
+	isLoggedIn: PropTypes.bool.isRequired,
 };
