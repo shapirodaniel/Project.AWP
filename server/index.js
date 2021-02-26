06867590ba4090fe646e1228ce56bafdf980b84d
shapirodaniel <shapirodanieladam@gmail.com>
2021-02-26 14:13:52 -0600

    update SSL config and cert/key, add to .gitignore

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const compression = require('compression')
-const session = require('express-session')
-const passport = require('passport')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const db = require('./db')
-const sessionStore = new SequelizeStore({db})
-const PORT = process.env.PORT || 3000
-const app = express()
-const socketio = require('socket.io')
-module.exports = app
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const session = require('express-session');
+const passport = require('passport');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const db = require('./db');
+const sessionStore = new SequelizeStore({ db });
+const PORT = process.env.PORT || 3000;
+const app = express();
+const socketio = require('socket.io');
+module.exports = app;
 
 // This is a global Mocha hook, used for resource cleanup.
 // Otherwise, Mocha v4+ never quits after tests.
 if (process.env.NODE_ENV === 'test') {
-  after('close the session store', () => sessionStore.stopExpiringSessions())
+	after('close the session store', () => sessionStore.stopExpiringSessions());
 }
 
 /**
@@ -26,119 +26,121 @@ if (process.env.NODE_ENV === 'test') {
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.NODE_ENV !== 'production') require('../secrets')
+if (process.env.NODE_ENV !== 'production') require('../secrets');
 
 // passport registration
-passport.serializeUser((user, done) => done(null, user.id))
+passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await db.models.user.findByPk(id)
-    done(null, user)
-  } catch (err) {
-    done(err)
-  }
-})
+	try {
+		const user = await db.models.user.findByPk(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
 
 const createApp = () => {
-  // logging middleware
-  app.use(morgan('dev'))
+	// logging middleware
+	app.use(morgan('dev'));
 
-  // body parsing middleware
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+	// body parsing middleware
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: true }));
 
-  // compression middleware
-  app.use(compression())
+	// compression middleware
+	app.use(compression());
 
-  // session middleware with passport
-  app.use(
-    session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
-      store: sessionStore,
-      resave: false,
-      saveUninitialized: false,
-    })
-  )
-  app.use(passport.initialize())
-  app.use(passport.session())
+	// session middleware with passport
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+			store: sessionStore,
+			resave: false,
+			saveUninitialized: false,
+		})
+	);
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-  // auth and api routes
-  app.use('/auth', require('./auth'))
-  app.use('/api', require('./api'))
+	// auth and api routes
+	app.use('/auth', require('./auth'));
+	app.use('/api', require('./api'));
 
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
+	// static file-serving middleware
+	app.use(express.static(path.join(__dirname, '..', 'public')));
 
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
+	// any remaining requests with an extension (.js, .css, etc.) send 404
+	app.use((req, res, next) => {
+		if (path.extname(req.path).length) {
+			const err = new Error('Not found');
+			err.status = 404;
+			next(err);
+		} else {
+			next();
+		}
+	});
 
-  // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
+	// sends index.html
+	app.use('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+	});
 
-  // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
-}
+	// error handling endware
+	app.use((err, req, res, next) => {
+		console.error(err);
+		console.error(err.stack);
+		res.status(err.status || 500).send(
+			err.message || 'Internal server error.'
+		);
+	});
+};
 
 const startListening = () => {
-  /**
-   * This is PeerServer JS's public STUN/TURN server
-   * which we'll set up to use SSL so we can
-   * connect to users on different devices
-   * than the one we're using to serve the app
-   */
+	/**
+	 * This is PeerServer JS's public STUN/TURN server
+	 * which we'll set up to use SSL so we can
+	 * connect to users on different devices
+	 * than the one we're using to serve the app
+	 */
 
-  const fs = require('fs')
-  const https = require('https')
-  const {PeerServer} = require('peer')
+	const fs = require('fs');
+	const https = require('https');
+	const { PeerServer } = require('peer');
 
-  const sslConfig = {
-    key: fs.readFileSync(path.resolve('server/server.key')),
-    cert: fs.readFileSync(path.resolve('server/server.crt')),
-  }
+	const sslConfig = {
+		key: fs.readFileSync(path.resolve('server.key')),
+		cert: fs.readFileSync(path.resolve('server.crt')),
+	};
 
-  const peerServer = PeerServer({
-    port: 9000,
-    path: '/',
-    ssl: sslConfig,
-  })
+	const peerServer = PeerServer({
+		port: 9000,
+		path: '/',
+		ssl: sslConfig,
+	});
 
-  const server = https.createServer(sslConfig, app)
-  server.listen(3000)
+	const server = https.createServer(sslConfig, app);
+	server.listen(3000);
 
-  // set up our socket control center
-  const io = socketio(server)
-  require('./socket')(io)
-}
+	// set up our socket control center
+	const io = socketio(server);
+	require('./socket')(io);
+};
 
-const syncDb = () => db.sync()
+const syncDb = () => db.sync();
 
 async function bootApp() {
-  await sessionStore.sync()
-  await syncDb()
-  await createApp()
-  await startListening()
+	await sessionStore.sync();
+	await syncDb();
+	await createApp();
+	await startListening();
 }
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
 // It will evaluate false when this module is required by another module - for example,
 // if we wanted to require our app in a test spec
 if (require.main === module) {
-  bootApp()
+	bootApp();
 } else {
-  createApp()
+	createApp();
 }
